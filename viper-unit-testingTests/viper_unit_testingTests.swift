62104c1fed748b8b6a1df2058a8401323f1977cb
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2020-09-03 20:07:21 +0700

    Initial Commit

---
+++ viper-unit-testingTests/viper_unit_testingTests.swift
@@ -0,0 +1,34 @@
+//
+//  viper_unit_testingTests.swift
+//  viper-unit-testingTests
+//
+//  Created by Ganang Arief Pratama on 03/09/20.
+//  Copyright © 2020 ganang.id. All rights reserved.
+//
+
+import XCTest
+@testable import viper_unit_testing
+
+class viper_unit_testingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
